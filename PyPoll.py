ca03bd2db7e89ae19942d7c0e21ec94558f1b59a
Addis de la Fuente <vicaenmayo@outlook.com>
2022-05-25 12:15:22 -0400

    Hello World in text file

--- PyPoll.py
+++ PyPoll.py
@@ -7,6 +7,7 @@
 
 # Import the datetime class from the datetime module.
 import datetime as dt
+from tkinter.filedialog import Open
 # Use the now() attribute on the datetime class to get the present time.
 now = dt.datetime.now()
 # Print the present time.
@@ -21,9 +22,47 @@ with open(file_to_load) as election_data:
 
     # to do: perform analysis
 
+    
+
 
 
     print(election_data)
+
+import csv
+
+import os
+
+# Assign a variable for the file to load and the path.
+
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Open the election results and read the file.
+
+with open(file_to_load) as election_data:
+
+
+    # Print the file object.
+    
+    print(election_data)
+
+
+# Create a filename variable to a direct or indirect path to the file.
+
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the open() function with the "w" mode we will write data to the file. 
+
+outfile = open(file_to_save, "w")
+
+# Write some data to the file.
+
+outfile.write("Hello World")
+
+# Close the file
+
+outfile.close()
+
+
 
 
 
